9ec776f256b7690f1f0502dcd4ee33d7ff473f94
Vitaliy Dmitriev <v.dmitriev@arenadata.io>
2025-10-15 13:15:24 +0300

    [ADH-5569] (4.0.1) Force a purge during table/partition drop in case if the 'external.table.purge' table option is present

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DropTableExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DropTableExec.scala
@@ -22,6 +22,7 @@ import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.connector.catalog.{Identifier, TableCatalog}
 import org.apache.spark.sql.errors.QueryCompilationErrors
 import org.apache.spark.util.ArrayImplicits._
+import org.apache.spark.sql.execution.command.CommandUtils.isPurgeableExternalTable
 
 /**
  * Physical plan node for dropping a table.
@@ -36,7 +37,11 @@ case class DropTableExec(
   override def run(): Seq[InternalRow] = {
     if (catalog.tableExists(ident)) {
       invalidateCache()
-      if (purge) catalog.purgeTable(ident) else catalog.dropTable(ident)
+      if (purge || isPurgeableExternalTable(catalog.loadTable(ident))) {
+        catalog.purgeTable(ident)
+      } else {
+        catalog.dropTable(ident)
+      }
     } else if (!ifExists) {
       throw QueryCompilationErrors.noSuchTableError(
         (catalog.name() +: ident.namespace() :+ ident.name()).toImmutableArraySeq)
