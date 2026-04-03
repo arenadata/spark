82cd15f28a1dca9bca3e763bedb25328f5d24352
Vitaliy Dmitriev <v.dmitriev@arenadata.io>
2026-04-03 18:45:19 +0300

    ADH-4718: support KYUUBI_AUTH for remote mode in python/pyspark/shell.py

--- python/pyspark/shell.py
+++ python/pyspark/shell.py
@@ -48,8 +48,18 @@ if getattr(builtins, "__IPYTHON__", False):
 
 if is_remote():
     try:
-        # Creates pyspark.sql.connect.SparkSession.
-        spark = SparkSession.builder.getOrCreate()
+        if os.environ.get("KYUUBI_AUTH"):
+            from kyuubi_spark_connect import KyuubiChannelBuilder
+            from pyspark.sql.connect.session import SparkSession as ConnectSparkSession
+            _kyuubi_builder = KyuubiChannelBuilder(
+                os.environ["SPARK_REMOTE"],
+                auth=os.environ.get("KYUUBI_AUTH", "kerberos"),
+                username=os.environ.get("KYUUBI_USERNAME"),
+                password=os.environ.get("KYUUBI_PASSWORD"))
+            spark = ConnectSparkSession(connection=_kyuubi_builder)
+        else:
+            # Creates pyspark.sql.connect.SparkSession.
+            spark = SparkSession.builder.getOrCreate()
     except Exception:
         import sys
         import traceback
