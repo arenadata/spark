dc47cc8cf89d8354be21137286891e3b42ecc95e
Vitaliy Dmitriev <v.dmitriev@arenadata.io>
2026-04-03 19:12:59 +0300

    ADH-4718: added python/pyspark/kyuubi/kyuubi_spark_connect.py (KyuubiChannelBuilder class)

--- python/pyspark/shell.py
+++ python/pyspark/shell.py
@@ -49,7 +49,7 @@ if getattr(builtins, "__IPYTHON__", False):
 if is_remote():
     try:
         if os.environ.get("KYUUBI_AUTH"):
-            from kyuubi_spark_connect import KyuubiChannelBuilder
+            from pyspark.kyuubi.kyuubi_spark_connect import KyuubiChannelBuilder
             from pyspark.sql.connect.session import SparkSession as ConnectSparkSession
             _kyuubi_builder = KyuubiChannelBuilder(
                 os.environ["SPARK_REMOTE"],
